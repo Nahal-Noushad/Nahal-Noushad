69fd2d7a391b1cbb0c74f85bf2f626ec546a5c9f
Nahal-Noushad <nn895@uowmail.edu.au>
2024-09-25 16:15:45 +0400

    Add files via upload

---
+++ Untitled-1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main(){}
+/*ESPRESSO INGREDIENT*/
+#define e_c 8
+#define e_w 30
+#define e_m 0
+#define e_choc 0
+#define e_p 3.5
+
+/*CAPPUCCINO INGREDIENTS*/
+#define c_c 8
+#define c_w 30
+#define c_m 70
+#define c_cho 0
+#define c_p 4.5
+
+/*MOCHA INGREDIENTS*/
+#define m_c 8
+#define m_w 39
+#define m_m 160
+#define m_cho 30
+#define m_p 5.5
+
+
+int menu_main(int value)
+{
+
+}
+
